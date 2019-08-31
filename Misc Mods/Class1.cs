52c2384d462f025a056da4c885f14b3dca3161bc
Arniel <aceba1@outlook.com>
2019-08-31 16:30:36 -0400

    Remove obsolete code

--- Misc Mods/Class1.cs
+++ Misc Mods/Class1.cs
@@ -92,112 +92,6 @@ namespace Misc_Mods
                         log = "Right-click on a block you would like to export, or choose to export your tech";
                     }
                 }
-                try
-                {
-                    if (Input.GetKeyDown(this.ForceGroundToggle))
-                    {
-                        this.TechGrounding = !this.TechGrounding;
-                    }
-                    if (this.TechGrounding)
-                    {
-                        Singleton.playerTank.grounded = true;
-                    }
-                    if (Input.GetKey(this.ForceGround))
-                    {
-                        Singleton.playerTank.grounded = true;
-                    }
-                    if (Input.GetKeyUp(this.ForceGround))
-                    {
-                        this.TechGrounding = false;
-                    }
-                }
-                catch
-                {
-                    this.TechGrounding = false;
-                }
-
-                /*if (Input.GetKeyDown(this.ForceThrustToggle))
-                {
-                    this.ForceThrust = !this.ForceThrust;
-                    if (!this.ForceThrust)
-                    {
-                        this.ForceThrustAmountForward = 0f;
-                        this.ForceThrustAmountUpward = 0f;
-                    }
-                }
-                if (this.ForceThrust)
-                {
-                    if (Input.GetKey(this.ForceThrustAddForward))
-                    {
-                        this.ForceThrustAmountForward += this.ThrustChange;
-                        if (this.ForceThrustAmountForward > 1f)
-                        {
-                            this.ForceThrustAmountForward = 1f;
-                        }
-                    }
-                    else if (Input.GetKey(this.ForceThrustRemoveForward))
-                    {
-                        this.ForceThrustAmountForward -= this.ThrustChange;
-                        if (this.ForceThrustAmountForward < -1f)
-                        {
-                            this.ForceThrustAmountForward = -1f;
-                        }
-                    }
-                    if (Input.GetKey(this.ForceThrustAddUpward))
-                    {
-                        this.ForceThrustAmountUpward += this.ThrustChange;
-                        if (this.ForceThrustAmountUpward > 1f)
-                        {
-                            this.ForceThrustAmountUpward = 1f;
-                        }
-                    }
-                    else if (Input.GetKey(this.ForceThrustRemoveUpward))
-                    {
-                        this.ForceThrustAmountUpward -= this.ThrustChange;
-                        if (this.ForceThrustAmountUpward < -1f)
-                        {
-                            this.ForceThrustAmountUpward = -1f;
-                        }
-                    }
-                }*/
-
-                if (Input.GetKeyDown(this.ForceAnchor))
-                {
-                    this.anchorcache = (Singleton.playerTank.IsAnchored ? 2 : 1);
-                }
-                if (Input.GetKey(this.ForceAnchor) && this.anchorcache == (Singleton.playerTank.IsAnchored ? 2 : 1))
-                {
-                    try
-                    {
-                        if (Singleton.playerTank.IsAnchored)
-                        {
-                            Singleton.playerTank.Anchors.UnanchorAll(false, false);
-                            this.anchorcache = ((this.anchorcache == (Singleton.playerTank.IsAnchored ? 2 : 1)) ? 2 : 0);
-                        }
-                        else
-                        {
-                            Vector3 position = Singleton.playerTank.transform.position;
-                            Quaternion rotation = Singleton.playerTank.transform.rotation;
-                            Vector3 velocity = Singleton.playerTank.rbody.velocity;
-                            Vector3 angularVelocity = Singleton.playerTank.rbody.angularVelocity;
-                            Singleton.playerTank.Anchors.TryAnchorAll(false);
-                            if (this.anchorcache == (Singleton.playerTank.IsAnchored ? 2 : 1))
-                            {
-                                Singleton.playerTank.transform.position = position;
-                                Singleton.playerTank.transform.rotation = rotation;
-                                Singleton.playerTank.rbody.velocity = velocity;
-                                Singleton.playerTank.rbody.angularVelocity = angularVelocity;
-                            }
-                            else
-                            {
-                                this.anchorcache = 0;
-                            }
-                        }
-                    }
-                    catch
-                    {
-                    }
-                }
             }
             catch (Exception E)
             {
@@ -209,54 +103,12 @@ namespace Misc_Mods
 
         private void MiscPage(int ID)
         {
-            SelectedPage = GUILayout.SelectionGrid(SelectedPage, new string[] { "Keybinds", "ModelExport" }, 3);
+            SelectedPage = GUILayout.SelectionGrid(SelectedPage, new string[] { "ModelExport" }, 3);
             switch (SelectedPage)
             {
-                case 0: KeybindPage(ID); break;
-                case 1: ExportPage(ID); break;
+                case 0: ExportPage(ID); break;
             }
             GUI.DragWindow();
-        }
-
-        private void KeybindPage(int ID)
-        {
-            if (this.InputIDToChange != 0)
-            {
-                Event current = Event.current;
-                if (current.isKey)
-                {
-                    switch (InputIDToChange)
-                    {
-                        case 1: this.ForceGround = current.keyCode; break;
-                        case 2: this.ForceGroundToggle = current.keyCode; break;
-                        case 3: this.ForceAnchor = current.keyCode; break;
-                    }
-                    this.InputIDToChange = 0;
-                }
-            }
-            ScrollPos = GUILayout.BeginScrollView(ScrollPos);
-
-            GUI.Label(new Rect(0f, 0f, 500f, 20f), "Force Ground Controls");
-            if (GUI.Button(new Rect(5f, 20f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 1) ? "Press a key to use" : this.ForceGround.ToString()))
-            {
-                this.InputIDToChange = 1;
-            }
-            GUI.Label(new Rect(0f, 40f, 500f, 20f), "Toggle Force Ground Controls");
-            if (GUI.Button(new Rect(5f, 60f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 2) ? "Press a key to use" : this.ForceGroundToggle.ToString()))
-            {
-                this.InputIDToChange = 2;
-            }
-            GUI.Label(new Rect(0f, 80f, 500f, 20f), "Force (Un)Anchor");
-            if (GUI.Button(new Rect(5f, 100f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 3) ? "Press a key to use" : this.ForceAnchor.ToString()))
-            {
-                this.InputIDToChange = 3;
-            }
-            if (GUI.Button(new Rect(5, 120, this.WindowRect.width * 0.4f, 20), "CRASH GAME")) 
-            {
-                Tony.PokeTony();
-            }
-
-            GUILayout.EndScrollView();
         }
 
         public static string log = "";
@@ -313,99 +165,6 @@ namespace Misc_Mods
             }
             GUILayout.EndScrollView();
         }
-
-        /* //There is nothing to see here anymore...
-        private void TurbinePage(int ID)
-        {
-            if (this.InputIDToChange != 0)
-            {
-                Event current = Event.current;
-                if (current.isKey)
-                {
-                    switch (InputIDToChange)
-                    {
-                        case 4: this.ForceThrustToggle = current.keyCode; break;
-                        case 5: this.ForceThrustAddForward = current.keyCode; break;
-                        case 6: this.ForceThrustRemoveForward = current.keyCode; break;
-                        case 7: this.ForceThrustAddUpward = current.keyCode; break;
-                        case 8: this.ForceThrustRemoveUpward = current.keyCode; break;
-                    }
-                    this.InputIDToChange = 0;
-                }
-            }
-
-            ScrollPos = GUI.BeginScrollView(new Rect(5, 40, WindowRect.width - 10, WindowRect.height - 40), ScrollPos, TurbineScrollRect);
-
-            this.ForceThrust = GUI.Toggle(new Rect(0f, 0f, 500f, 20f), this.ForceThrust, "Toggle Force turbine");
-            if (GUI.Button(new Rect(5f, 20f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 4) ? "Press a key to use" : this.ForceThrustToggle.ToString()))
-            {
-                this.InputIDToChange = 4;
-            }
-            GUI.Label(new Rect(30f, 40f, 500f, 20f), "Change turbine thrust rate (" + this.ThrustChange.ToString() + ")");
-            this.ThrustChange = GUI.HorizontalSlider(new Rect(15f, 65f, this.WindowRect.width - 65f, 20f), this.ThrustChange, 1f, 0f);
-            GUI.Label(new Rect(30f, 60f, 500f, 20f), "Turbine Amount Forward (" + this.ForceThrustAmountForward.ToString() + ")");
-            this.ForceThrustAmountForward = GUI.HorizontalSlider(new Rect(15f, 100f, this.WindowRect.width - 65f, 20f), this.ForceThrustAmountForward, 1f, -1f);
-            GUI.Label(new Rect(30f, 120f, 500f, 20f), "Add turbine thrust Forward");
-            if (GUI.Button(new Rect(35f, 140f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 5) ? "Press a key to use" : this.ForceThrustAddForward.ToString()))
-            {
-                this.InputIDToChange = 5;
-            }
-            GUI.Label(new Rect(30f, 160f, 500f, 20f), "Remove turbine thrust Forward");
-            if (GUI.Button(new Rect(35f, 180f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 6) ? "Press a key to use" : this.ForceThrustRemoveForward.ToString()))
-            {
-                this.InputIDToChange = 6;
-            }
-            GUI.Label(new Rect(30f, 200f, 500f, 20f), "Turbine Amount Upward (" + this.ForceThrustAmountUpward.ToString() + ")");
-            this.ForceThrustAmountUpward = GUI.HorizontalSlider(new Rect(15f, 220f, this.WindowRect.width - 65f, 20f), this.ForceThrustAmountUpward, 1f, -1f);
-            GUI.Label(new Rect(30f, 240f, 500f, 20f), "Add turbine thrust Upward");
-            if (GUI.Button(new Rect(35f, 260f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 7) ? "Press a key to use" : this.ForceThrustAddUpward.ToString()))
-            {
-                this.InputIDToChange = 7;
-            }
-            GUI.Label(new Rect(30f, 280f, 500f, 20f), "Remove turbine thrust Upward");
-            if (GUI.Button(new Rect(35f, 300f, this.WindowRect.width * 0.5f, 20f), (this.InputIDToChange == 8) ? "Press a key to use" : this.ForceThrustRemoveUpward.ToString()))
-            {
-                this.InputIDToChange = 8;
-            }
-            GUI.EndScrollView();
-        }
-        /**/
-
-        //private void FixedUpdate()
-        //{
-        //    if (this.ForceThrust)
-        //    {
-        //        try
-        //        {
-        //            foreach (FanJet fanJet in Singleton.playerTank.GetComponentsInChildren<FanJet>())
-        //            {
-        //                Vector3 vector = Quaternion.Inverse(Singleton.playerTank.control.FirstController.block.transform.rotation) * Vector3.forward;
-        //                if (vector.z > 0.8f)
-        //                {
-        //                    fanJet.SetSpin(-this.ForceThrustAmountForward);
-        //                }
-        //                else if (vector.z < -0.8f)
-        //                {
-        //                    fanJet.SetSpin(this.ForceThrustAmountForward);
-        //                }
-        //                else if (vector.y > 0.8f)
-        //                {
-        //                    fanJet.SetSpin(-this.ForceThrustAmountUpward);
-        //                }
-        //                else if (vector.y < -0.8f)
-        //                {
-        //                    fanJet.SetSpin(this.ForceThrustAmountUpward);
-        //                }
-        //            }
-        //        }
-        //        catch
-        //        {
-        //            this.ForceThrust = false;
-        //            this.ForceThrustAmountForward = 0f;
-        //            this.ForceThrustAmountUpward = 0f;
-        //        }
-        //    }
-        //}
 
         internal class GUIDisplay : MonoBehaviour
         {
