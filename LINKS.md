aa90988c0ac21fb4ab087abc299ec9d8846273ab
Arniel <aceba1@outlook.com>
2019-08-31 16:31:51 -0400

    Clean up references

--- LINKS.md
+++ LINKS.md
@@ -1,2 +1,1 @@
 https://github.com/Aceba1/Misc-Mods/tree/master/Built
-/Aceba1/TTQMM-ModConfigHelper
--- Misc Mods/Class1.cs
+++ Misc Mods/Class1.cs
@@ -1,4 +1,4 @@
-﻿using ModHelper.Config;
+﻿//using ModHelper.Config;
 using System;
 using UnityEngine;
 
