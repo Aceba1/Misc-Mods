1c48260c785393a30d5cad5e9a0bfb3a3ee6d077
Arniel <aceba1@outlook.com>
2020-01-06 11:29:28 -0500

    Texture dumper

--- Misc Mods/Class1.cs
+++ Misc Mods/Class1.cs
@@ -152,6 +152,57 @@ namespace Misc_Mods
 
                         log = "Exported individual .obj files to " + path;
                     }
+                    if (GUILayout.Button("Export all textures"))
+                    {
+                        string path = "_Export/Textures/" + module.name;
+                        if (!System.IO.Directory.Exists(path))
+                        {
+                            System.IO.Directory.CreateDirectory(path);
+                        }
+
+                        Texture2D original = ManUI.inst.GetSprite(module.visible.m_ItemType).texture;
+                        Texture2D copy = duplicateTexture(original);
+                        System.IO.File.WriteAllBytes(path + "/icon.png", copy.EncodeToPNG());
+
+                        var type = ManSpawn.inst.GetCorporation(module.BlockType);
+                        var maintex = ManCustomSkins.inst.GetSkinTexture(type, 0);
+
+                        System.IO.File.WriteAllBytes(path + "/" + type.ToString() + "_1.png", duplicateTexture(maintex.m_Albedo).EncodeToPNG());
+                        System.IO.File.WriteAllBytes(path + "/" + type.ToString() + "_2.png", duplicateTexture(maintex.m_Metal).EncodeToPNG());
+                        System.IO.File.WriteAllBytes(path + "/" + type.ToString() + "_3.png", duplicateTexture(maintex.m_Emissive).EncodeToPNG());
+
+                        Dictionary<Texture, string> buffer = new Dictionary<Texture, string>();
+                        List<Vector2> invalid = new List<Vector2>();
+                        foreach (var mr in module.GetComponentsInChildren<Renderer>(true))
+                        {
+                            var mat = mr.sharedMaterial;
+
+                            foreach (string key in mat.shaderKeywords)
+                                if (key == "_SKINS") // Do not dump
+                                {
+                                    invalid.Add(new Vector2(mat.mainTexture.width, mat.mainTexture.height));
+                                    goto skipdump; // continue would not work here
+                                }
+
+                            var tex1 = mat.mainTexture;
+                            if (tex1 != null && !QuickCompare(buffer, tex1))//!buffer.ContainsKey(tex1))
+                                buffer.Add(tex1, mr.name + "_" + tex1.name + "_1.png");
+                            var tex2 = mat.GetTexture("_MetallicGlossMap");
+                            if (tex2 != null && !QuickCompare(buffer, tex2))//buffer.ContainsKey(tex2))
+                                buffer.Add(tex2, mr.name + "_" + tex2.name + "_2.png");
+                            var tex3 = mat.GetTexture("_EmissionMap");
+                            if (tex3 != null && !QuickCompare(buffer, tex3))//buffer.ContainsKey(tex3))
+                                buffer.Add(tex3, mr.name + "_" + tex3.name + "_3.png");
+                            skipdump:;
+                        }
+                        foreach (var tex in buffer)
+                        {
+                            if (invalid.Contains(new Vector2(tex.Key.width, tex.Key.height))) continue;
+                            System.IO.File.WriteAllBytes(path + "/" + tex.Value, duplicateTexture(tex.Key).EncodeToPNG());
+                        }
+
+                        log = "Exported all .png files to " + path;
+                    }
                 }
             }
             catch (Exception E)
@@ -160,6 +211,13 @@ namespace Misc_Mods
                 Console.WriteLine(E.Message);
                 Console.WriteLine(E.StackTrace);
             }
+        }
+
+        static bool QuickCompare(Dictionary<Texture, string> A, Texture B)
+        {
+            foreach (var a in A.Keys)
+                if (a.GetHashCode() == B.GetHashCode()) return true;
+            return false;
         }
 
         private void BlockInfoDumperPage(int ID)
